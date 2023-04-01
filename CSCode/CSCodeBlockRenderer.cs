c869cb6b2d119bc20ebcb3697de36d79948ce6d2
Dávid Czompó <czompi@czompi.hu>
2023-04-01 13:06:10 +0200

    Migrated from CWCT-MA repo to a separate one
    This state is representative of `Markdig.Extensions.Xmd` version `1.0.8`.

---
+++ CSCode/CSCodeBlockRenderer.cs
@@ -0,0 +1,13 @@
+﻿using Markdig.Renderers;
+using Markdig.Renderers.Html;
+
+namespace Markdig.Extensions.Xmd.CSCode;
+
+public class CSCodeBlockRenderer : HtmlObjectRenderer<CSCodeBlock>
+{
+
+    protected override void Write(HtmlRenderer renderer, CSCodeBlock obj)
+    {
+        renderer.Write($"{CodeHelper.ExecuteBlock(obj.SourceCode)}");
+    }
+}
---
+++ CSCode/CSCodeInlineRenderer.cs
@@ -0,0 +1,13 @@
+﻿using Markdig.Renderers;
+using Markdig.Renderers.Html;
+
+namespace Markdig.Extensions.Xmd.CSCode;
+
+public class CSCodeInlineRenderer : HtmlObjectRenderer<CSCodeInline>
+{
+
+    protected override void Write(HtmlRenderer renderer, CSCodeInline obj)
+    {
+        renderer.Write($"{CodeHelper.ExecuteInline(obj.SourceCode)}");
+    }
+}
